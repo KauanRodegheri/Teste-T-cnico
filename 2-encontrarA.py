7804b8e529477b01864ad69c64f9396e777e76fd
Kauan Rodegheri <kauan.rodegheri@gmail.com>
2024-09-28 11:04:07 -0300

    novas formas

--- 2-encontrarA.py
+++ 2-encontrarA.py
@@ -1,4 +1,5 @@
 string = str(input('digite sua string: '))
+#1° forma
 count=0
 if 'a' in string or 'A' in string:
     for i in string:
@@ -7,3 +8,24 @@ if 'a' in string or 'A' in string:
     print(f'letra A encontrada {count} vezes')
 else:
     print('Letra A não foi encontrada')
+
+#2° forma
+count=0
+for letra in string:
+    if letra in 'Aa':
+        count+=1
+if count >=1:
+    print(f'foram encontrados {count} letra A')
+else:
+    print('não tem A')
+
+#3° forma
+lista = []
+for letra in string:
+    if letra in 'Aa':
+        lista.append(letra)
+
+if lista:
+    print(f'foram encontrados {len(lista)} letra A')
+else:
+    print('não tem A')
